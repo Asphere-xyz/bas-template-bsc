64bb29268cc51247031b260234d956087d28aa87
Dmitry Savonin <dmitry@ankr.com>
2022-03-25 18:21:22 +0300

    fixed bug with passing raw memory into evm hooks

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -153,6 +153,12 @@ func RunPrecompiledContract(p PrecompiledContract, input []byte, suppliedGas uin
 		return nil, 0, ErrOutOfGas
 	}
 	suppliedGas -= gasCost
+	// special case for EVM hooks (they consume 0 gas), and we should be 100% sure that we use copy of the input
+	if gasCost == 0 {
+		newInput := make([]byte, len(input))
+		copy(newInput, input)
+		input = newInput
+	}
 	output, err := p.Run(input)
 	return output, suppliedGas, err
 }
